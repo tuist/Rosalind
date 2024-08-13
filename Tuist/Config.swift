84531b71f4e74251eff9fa57e9862ce59e6b0d99
Pedro <pedro@pepicrft.me>
2024-08-13 17:39:30 +0200

    Fix linting issues

--- Tuist/Config.swift
+++ Tuist/Config.swift
@@ -1,9 +1,9 @@
 import ProjectDescription
 
 let config = Config(
-//    Create an account with "tuist auth" and a project with "tuist project create"
+    //    Create an account with "tuist auth" and a project with "tuist project create"
 //    then uncomment the section below and set the project full-handle.
 //    * Read more: https://docs.tuist.io/guides/quick-start/gather-insights
 //
-   fullHandle: "tuist/apple-bundle-size-analyzer"
+    fullHandle: "tuist/apple-bundle-size-analyzer"
 )
