eba6ccd20ee11caad0f735d9b33f8a6ebde2b967
Pedro <pedro@pepicrft.me>
2025-03-04 20:33:49 +0100

    Include the shasum

--- Sources/Rosalind/Report.swift
+++ Sources/Rosalind/Report.swift
@@ -3,13 +3,20 @@ import JSONSchemaBuilder
 
 @Schemable
 public enum Report: Sendable, Codable {
-    indirect case app(path: String, size: Int, children: [Report])
-    indirect case unknown(path: String, size: Int, children: [Report])
+    indirect case app(path: String, size: Int, shasum: String, children: [Report])
+    indirect case unknown(path: String, size: Int, shasum: String, children: [Report])
 
     var size: Int {
         switch self {
-        case let .app(_, size, _): return size
-        case let .unknown(_, size, _): return size
+        case let .app(_, size, _, _): return size
+        case let .unknown(_, size, _, _): return size
+        }
+    }
+
+    var shasum: String {
+        switch self {
+        case let .app(_, _, shasum, _): return shasum
+        case let .unknown(_, _, shasum, _): return shasum
         }
     }
 }
--- Sources/Rosalind/Rosalind.swift
+++ Sources/Rosalind/Rosalind.swift
@@ -1,3 +1,5 @@
+import Command
+import CryptoKit
 import FileSystem
 import Foundation
 import Path
@@ -19,13 +21,18 @@ public protocol Rosalindable {
 
 public struct Rosalind: Rosalindable {
     let fileSystem: FileSysteming
+    let commandRunner: CommandRunning
 
     public init() {
-        self.init(fileSystem: FileSystem())
+        self.init(fileSystem: FileSystem(), commandRunner: CommandRunner())
     }
 
-    init(fileSystem: FileSysteming) {
+    init(
+        fileSystem: FileSysteming,
+        commandRunner: CommandRunning
+    ) {
         self.fileSystem = fileSystem
+        self.commandRunner = commandRunner
     }
 
     public func analyze(path: AbsolutePath) async throws -> Report {
@@ -36,9 +43,38 @@ public struct Rosalind: Rosalindable {
     public func traverse(path: AbsolutePath, normalizingPath: AbsolutePath) async throws -> Report {
         let children: [Report] = try await (try await fileSystem.glob(directory: path, include: ["*"]).collect()).sorted()
             .asyncMap { try await traverse(path: $0, normalizingPath: normalizingPath) }
+        let size = try await size(path: path, children: children)
+        let shasum = try await shasum(path: path, children: children)
+        if path.extension == "app" {
+            return .app(path: path.relative(to: normalizingPath).pathString, size: size, shasum: shasum, children: children)
+        } else {
+            return .unknown(path: path.relative(to: normalizingPath).pathString, size: size, shasum: shasum, children: children)
+        }
+    }
 
+    private func shasum(path: AbsolutePath, children: [Report]) async throws -> String {
+        if try await fileSystem.exists(path, isDirectory: true) { // Directory
+            let sortedHashes = children.map(\.shasum).sorted()
+            let combinedString = sortedHashes.joined()
+            guard let data = combinedString.data(using: .utf8) else {
+                return ""
+            }
+            let digest = SHA256.hash(data: data)
+            return digest.compactMap { String(format: "%02x", $0) }.joined()
+        } else {
+            return String(
+                (
+                    try await commandRunner.run(arguments: ["/usr/bin/shasum", "-a", "256", path.pathString])
+                        .concatenatedString(including: Set([.standardOutput]))
+                ).trimmingCharacters(in: .whitespacesAndNewlines)
+                    .split(separator: " ")[0]
+            )
+        }
+    }
+
+    private func size(path: AbsolutePath, children: [Report]) async throws -> Int {
         var size = 0
-        if try await fileSystem.exists(path, isDirectory: true) {
+        if try await fileSystem.exists(path, isDirectory: true) { // Directory
             size = children.reduce(Int(0)) { acc, next in
                 var acc = acc
                 acc += next.size
@@ -47,11 +83,6 @@ public struct Rosalind: Rosalindable {
         } else {
             size = ((try FileManager.default.attributesOfItem(atPath: path.pathString))[.size] as? Int) ?? 0
         }
-
-        if path.extension == "app" {
-            return .app(path: path.relative(to: normalizingPath).pathString, size: size, children: children)
-        } else {
-            return .unknown(path: path.relative(to: normalizingPath).pathString, size: size, children: children)
-        }
+        return size
     }
 }
